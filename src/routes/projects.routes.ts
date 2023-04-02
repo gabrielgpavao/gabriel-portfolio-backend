1a2d4481a77f9ab2a6a5b99c6b062782c7706009
Gabriel <gabpavao@outlook.com>
2023-04-01 21:05:09 -0300

    feat: verify duplicity values in link and repository urls with a middleware

--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 import { createProjectController } from '../controllers/projects.controllers'
-import { validateInputDataMiddleware } from '../middlewares'
+import { validateInputDataMiddleware, verifyUrlDuplicityMiddleware } from '../middlewares/projects.middlewares'
 import { inputProjectDataSchema } from '../schemas/projects.schemas'
 
 export const projectsRoutes: Router = Router()
 
-projectsRoutes.post('', validateInputDataMiddleware(inputProjectDataSchema), createProjectController)
+projectsRoutes.post('', validateInputDataMiddleware(inputProjectDataSchema), verifyUrlDuplicityMiddleware, createProjectController)
