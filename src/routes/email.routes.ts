0e24d9196bd079ab63167ce7efb7d6ad3cc6278c
Gabriel <gabpavao@outlook.com>
2023-04-08 14:33:17 -0300

    feat: handle cors policy

--- src/routes/email.routes.ts
+++ src/routes/email.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { sendEmailController } from '../controllers/email.controllers';
+import cors from 'cors'
 
 export const emailRoutes: Router = Router()
 
-emailRoutes.post('', sendEmailController)
+emailRoutes.post('', cors(), sendEmailController)
